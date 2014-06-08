cede6083632bdf96bf5947e9b8ec8d732d2e37ab
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2014-06-08 12:55:31 +0200

    added helper method

--- SmartMapperModule/src/test/java/de/crawling/spider/idea/plugin/mapper/TestHelper.java
+++ SmartMapperModule/src/test/java/de/crawling/spider/idea/plugin/mapper/TestHelper.java
@@ -10,7 +10,7 @@ import static org.mockito.Mockito.when;
  */
 public class TestHelper {
 
-    protected PsiMethod[] createMethodTestData() {
+    protected PsiMethod[] createInputMethodTestData() {
         PsiMethod[] methods = new PsiMethod[4];
 
         PsiMethod m0 = mock(PsiMethod.class);
@@ -28,6 +28,23 @@ public class TestHelper {
         PsiMethod m3 = mock(PsiMethod.class);
         when(m3.getName()).thenReturn("setDoNotReturn");
         methods[3] = m3;
+        return methods;
+    }
+
+    protected PsiMethod[] createOutputMethodTestData() {
+        PsiMethod[] methods = new PsiMethod[3];
+
+        PsiMethod m0 = mock(PsiMethod.class);
+        when(m0.getName()).thenReturn("setMethod");
+        methods[0] = m0;
+
+        PsiMethod m1 = mock(PsiMethod.class);
+        when(m1.getName()).thenReturn("setMethod1");
+        methods[1] = m1;
+
+        PsiMethod m2 = mock(PsiMethod.class);
+        when(m2.getName()).thenReturn("setMethod2");
+        methods[2] = m2;
         return methods;
     }
 }
