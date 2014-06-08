c4364a974ba25cc4f640ed1a43120a0e65c9aa8b
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2014-06-08 07:43:07 +0200

    Properties for mapper to collect all parameters in one class

---
+++ SmartMapperModule/src/de/crawling/spider/idea/plugin/mapper/MapperProperties.java
@@ -0,0 +1,94 @@
+package de.crawling.spider.idea.plugin.mapper;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiMethod;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.apache.commons.lang.StringUtils.*;
+
+/**
+ * Created by sscheffler on 08.06.14.
+ */
+public class MapperProperties {
+
+    private String setterCanonicalClassName;
+    private String getterCanonicalClassName;
+    private List<PsiMethod> selectedMethods;
+    private Project project;
+    private boolean loadSuperClassMethods;
+    private boolean defaultValues;
+
+    public MapperProperties(String setterCanonicalClassName, String getterCanonicalClassName, List<PsiMethod> selectedMethods, Project project, boolean loadSuperClassMethods, boolean defaultValues) {
+        this.setterCanonicalClassName = setterCanonicalClassName;
+        this.getterCanonicalClassName = getterCanonicalClassName;
+        this.selectedMethods = selectedMethods;
+        this.project = project;
+        this.loadSuperClassMethods = loadSuperClassMethods;
+        this.defaultValues = defaultValues;
+    }
+
+    public String getSetterCanonicalClassName() {
+        return setterCanonicalClassName;
+    }
+
+    public void setSetterCanonicalClassName(String setterCanonicalClassName) {
+        this.setterCanonicalClassName = setterCanonicalClassName;
+    }
+
+    public String getGetterCanonicalClassName() {
+        return getterCanonicalClassName;
+    }
+
+    public void setGetterCanonicalClassName(String getterCanonicalClassName) {
+        this.getterCanonicalClassName = getterCanonicalClassName;
+    }
+
+    public List<PsiMethod> getSelectedMethods() {
+        return selectedMethods;
+    }
+
+    public void setSelectedMethods(List<PsiMethod> selectedMethods) {
+        this.selectedMethods = selectedMethods;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public boolean isLoadSuperClassMethods() {
+        return loadSuperClassMethods;
+    }
+
+    public void setLoadSuperClassMethods(boolean loadSuperClassMethods) {
+        this.loadSuperClassMethods = loadSuperClassMethods;
+    }
+
+    public boolean isDefaultValues() {
+        return defaultValues;
+    }
+
+    public void setDefaultValues(boolean defaultValues) {
+        this.defaultValues = defaultValues;
+    }
+
+    public boolean propertiesValid(){
+        return (isNotBlank(setterCanonicalClassName) &&
+                null != selectedMethods &&
+                !selectedMethods.isEmpty() &&
+                null!=project );
+    }
+
+    public boolean propertiesValidForGetter(){
+        return (propertiesValid() &&
+                isNotBlank(getterCanonicalClassName));
+    }
+
+
+}
