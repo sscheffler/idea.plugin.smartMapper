8805b946578d149e907d10b0e3bf2950b5a5ae0f
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2014-06-09 09:03:26 +0200

    enum for default types

--- SmartMapperModule/src/main/java/de/crawling/spider/idea/plugin/mapper/produce/DefaultMethodParameterValueProducer.java
+++ SmartMapperModule/src/main/java/de/crawling/spider/idea/plugin/mapper/produce/DefaultMethodParameterValueProducer.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * TODO: Produces default values for method parameters
+ * Produces default values for method parameters
  *
  * @author sscheffler(stefan-scheffler@web.de)
  * @date 09.06.14
@@ -28,53 +28,92 @@ public class DefaultMethodParameterValueProducer {
      * @param setterMethodName
      * @return
      */
-    public String produceDefaultValue(String qualifiedClassName, String setterMethodName) {
+    public String produceDefaultValueForNonPrimitives(String qualifiedClassName, String setterMethodName) {
         String returnValue = "";
         try {
             Class parameterTypeClass = Class.forName(qualifiedClassName);
-
-            LOGGER.trace("Type [{}] - primitive [{}]", qualifiedClassName, parameterTypeClass.isPrimitive());
-            if(!parameterTypeClass.isPrimitive()){
-                returnValue = evaluateNonPrimitives(parameterTypeClass.getCanonicalName(), setterMethodName);
-            }else{
-                returnValue = evaluatePrimitives(parameterTypeClass.getCanonicalName());
-            }
-
-            LOGGER.trace("check if type is an array");
+            returnValue = evaluateNonPrimitives(parameterTypeClass.getCanonicalName(), setterMethodName);
         }catch(ClassNotFoundException cne){
             LOGGER.error("Class [{}] was not found", qualifiedClassName);
-            return "";
         }
 
         return returnValue;
     }
 
-    /**
-     * builds default value for primitive types
-     * @param canonicalName
-     * @return
-     */
-    private String evaluatePrimitives(String canonicalName) {
 
-        return null;
+    private String evaluatePrimitives(String name) {
+
+        switch (name){
+            case "java.lang.String":
+            default: return "";
+        }
+
     }
 
     private String evaluateNonPrimitives(final String name, String setterMethodName){
 
-        String returnValue = "";
-
         //TODO: implement more cases / recursive constructor filling [sscheffler(stefan-scheffler@web.de) - 09.06.14 - 07:56]
         switch (name){
-            case "java.lang.String":{
-                returnValue = "\"" + regexUtil.calculateDefaultValueFromSetter(setterMethodName) + "\"";
-                break;
-            }
-            default: returnValue = "";//"new " +name+ "( )";
+            case "java.lang.String":
+                return "\"" + regexUtil.calculateDefaultValueFromSetter(setterMethodName) + "\"";
+            case "java.lang.Boolean":
+                return "true";
+
+            default: return "";//"new " +name+ "( )";
         }
 
-        return returnValue;
     }
 
-    public String produceDefaultValueFromKeyWord(String text) {
+    /**
+     * builds default value for primitive types
+     * @param typeName
+     * @return
+     */
+    public String produceDefaultValueForPrimitives(String typeName) {
+
+        return evaluatePrimitives(typeName);
     }
+}
+
+enum DefaultTypes{
+    BOOLEAN("boolean", Boolean.class.getCanonicalName(), true),
+    CHAR("char", Character.class.getCanonicalName(),'a'),
+    BYTE("byte", Byte.class.getCanonicalName(), "b".getBytes()[0]),
+    SHORT("short", Short.class.getCanonicalName(), 1),
+    INTEGER("int", Integer.class.getCanonicalName(), 1),
+    LONG("long", Long.class.getCanonicalName(), 1L),
+    FLOAT("float", Float.class.getCanonicalName(), 1.0),
+    DOUBLE("double", Double.class.getCanonicalName(), 1.0);
+
+    private final String primName;
+    private final String complName;
+    private final Object defaultValue;
+
+    DefaultTypes(String primName, String complName, Object defaultValue) {
+        this.primName = primName;
+        this.complName = complName;
+        this.defaultValue = defaultValue;
+    }
+
+    public String getPrimName() {
+        return primName;
+    }
+
+    public String getComplName() {
+        return complName;
+    }
+
+    public Object getDefaultValue() {
+        return defaultValue;
+    }
+
+    public DefaultTypes fromValue(String str){
+        for(DefaultTypes type : DefaultTypes.values()){
+            if(type.getPrimName().equals(str) || type.complName.equals(str)){
+                return type;
+            }
+        }
+        return null;
+    }
+
 }
